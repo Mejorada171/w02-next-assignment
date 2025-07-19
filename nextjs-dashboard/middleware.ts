c9d735c25de6aecddf30885b0d37a2454150072f
jeremyaaronha <jeremyaaronha@gmail.com>
2025-07-19 01:49:33 -0400

    solucionadno producccion

--- nextjs-dashboard/middleware.ts
+++ nextjs-dashboard/middleware.ts
@@ -1,10 +1,7 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-export const runtime = 'nodejs';
- 
-export default NextAuth(authConfig).auth;
- 
+export { auth as default } from '@/auth';
+
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|.*\\.png$|favicon.ico|login).*)',
+  ],
 };
