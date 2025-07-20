ea4edcc5f50ff2424953ce0265f8ae1f4014956c
jeremyaaronha <jeremyaaronha@gmail.com>
2025-07-20 00:25:38 -0400

    dejar original pero con comentarios de otro

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -19,3 +19,32 @@ export const authConfig = {
   },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
+
+
+// import type { NextAuthConfig } from 'next-auth';
+// import { NextResponse } from 'next/server';
+
+// export const authConfig = {
+//   pages: {
+//     signIn: '/login',
+//   },
+//   callbacks: {
+//     authorized({ auth, request }) {
+//       const isLoggedIn = !!auth?.user;
+//       const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard');
+
+//       if (isOnDashboard) {
+//         return isLoggedIn;
+//       }
+
+//       if (isLoggedIn) {
+//         return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
+//       }
+
+//       return true;
+//     },
+//   },
+//   providers: [],
+// } satisfies NextAuthConfig;
+
+// export default authConfig;
