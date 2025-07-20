82c04dfb5f04d04a191925af4b155dc9c40b4d89
jeremyaaronha <jeremyaaronha@gmail.com>
2025-07-19 22:36:02 -0400

    verificar deploy vercel

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,27 +1,22 @@
+import type { NextRequest } from 'next/server';
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({
-      auth,
-      request,
-    }: {
-      auth: any;
-      request: { nextUrl: URL };
-    }) {
+    authorized({ auth, request }: { auth: any; request: NextRequest }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard');
+
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false;
+        return isLoggedIn;
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', request.nextUrl));
       }
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [],
 } satisfies NextAuthConfig;
